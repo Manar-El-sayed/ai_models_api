4cb659ef7317dfeb521e2bc16aade845ec25e909
Manar-El-sayed <manarelsayed542>
2025-05-03 00:36:13 +0300

    Second commit: Add requirements

--- main.py
+++ main.py
@@ -2952,5 +2952,7 @@ async def predict_cancer(data: CancerInput):
         logger.error(f"Prediction error: {str(e)}", exc_info=True)
         raise HTTPException(status_code=500, detail=f"Prediction error: {str(e)}")
     
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
